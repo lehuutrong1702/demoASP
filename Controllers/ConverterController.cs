6a5dbc7c9fcf38918874f338eb1cab1969816e61
trong <lehuutrong141@gmail.com>
2024-10-12 15:21:52 +0700

    Add project files.

---
+++ Controllers/ConverterController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers {
+    public class ConverterController : Controller {
+        public IActionResult convert(double usd) {
+            Models.Converter converter = new Models.Converter();
+            converter.convertUSDtoVND(usd);
+            return View("myview",converter);
+        }
+        public IActionResult Index()
+            {
+             return View("myview");
+            }
+    }
+}
---
+++ Models/Converter.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstProject.Models {
+    public class Converter {
+            
+       private static readonly double rate = 24610; // 1 usd = rate vnd 
+
+        public double result ;
+        public void convertUSDtoVND(double _value ) {
+            result  = _value * rate ;
+        }
+
+    }
+}
